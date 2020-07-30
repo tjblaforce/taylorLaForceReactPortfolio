c788d7f1c57a986caf0316b1f753195055105dfa
tjblaforce <tjblaforce@gmail.com>
2020-07-30 11:52:52 -0600

    commit just before styling

---
+++ src/components/portfolio/portfolio-item.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function(props) {
+    // Data that we'll need:
+    // - background image: thumb_image_url
+    // -logo: logo_url
+    // -description: description
+    // - id: id
+
+    const { id, description, thumb_image_url, logo_url } = props.item;
+    return (
+        <div>
+            <img src={thumb_image_url} />
+            <img src={logo_url} />
+            <div>
+                {description}
+            </div>
+
+            <Link to={`/portfolio/${id}`}> Link </Link>
+
+        </div>
+    )
+}
